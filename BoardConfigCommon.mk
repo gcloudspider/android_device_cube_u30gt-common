5a115b740cd52ac88c3a879f802c254902e8dacd
fonix232 <fonix232@gmail.com>
2012-07-31 14:58:14 +0200

    Initial commit

---
+++ BoardConfigCommon.mk
@@ -0,0 +1,80 @@
+#
+# Copyright (C) 2012 The Android Open-Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# This variable is set first, so it can be overridden
+# by BoardConfigVendor.mk
+BOARD_USES_GENERIC_AUDIO := true
+USE_CAMERA_STUB := true
+
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+
+TARGET_NO_BOOTLOADER := true
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_ARCH_VARIANT := armv7-a
+TARGET_ARCH_VARIANT_CPU := cortex-a9
+TARGET_ARCH_VARIANT_FPU := vfpv3-d16
+TARGET_CPU_SMP := true
+ARCH_ARM_HAVE_TLS_REGISTER := true
+
+BOARD_KERNEL_BASE := 0x60400000
+BOARD_KERNEL_CMDLINE := 
+BOARD_PAGE_SIZE := 16384
+
+TARGET_NO_RADIOIMAGE := true
+TARGET_BOARD_PLATFORM := rk30xx
+TARGET_BOOTLOADER_BOARD_NAME := u30gt
+
+BOARD_EGL_CFG := device/cube/u30gt-common/egl.cfg
+
+#BOARD_USES_HGL := true
+#BOARD_USES_OVERLAY := true
+USE_OPENGL_RENDERER := true
+
+BOARD_USES_AUDIO_LEGACY := true
+
+TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
+
+BOARD_CUSTOM_RECOVERY_KEYMAPPING := $(LOCAL_PATH)/recovery/recovery_ui.c
+BOARD_HAS_NO_SELECT_BUTTON := true
+BOARD_HAS_LARGE_FILESYSTEM := true
+
+# override recovery init.rc
+TARGET_RECOVERY_INITRC := device/cube/u30gt/recovery/init.rc
+
+# Indicate that the board has an Internal SD Card
+BOARD_HAS_SDCARD_INTERNAL := true
+
+# device-specific extensions to the updater binary
+TARGET_USERIMAGES_USE_EXT4 := true
+BOARD_FLASH_BLOCK_SIZE := 131072
+
+WPA_SUPPLICANT_VERSION := VER_0_8_X
+BOARD_WPA_SUPPLICANT_DRIVER := NL80211
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_bcmdhd
+BOARD_HOSTAPD_DRIVER := NL80211
+BOARD_HOSTAPD_PRIVATE_LIB := lib_driver_cmd_bcmdhd
+BOARD_WLAN_DEVICE := RK903
+
+WIFI_DRIVER_MODULE_PATH     := "/system/lib/modules/wlan.ko"
+WIFI_DRIVER_FW_PATH_PARAM   := "/sys/module/wlan/parameters/firmware_path"
+WIFI_DRIVER_FW_PATH_STA     := "/system/etc/firmware/fw_RK903.bin"
+WIFI_DRIVER_FW_PATH_AP      := "/system/etc/firmware/fw_RK903_apsta.bin"
+WIFI_DRIVER_FW_PATH_P2P     := "/system/etc/firmware/fw_RK903_p2p.bin"
+WIFI_DRIVER_MODULE_NAME     := "RK903"
+WIFI_DRIVER_MODULE_ARG      := "iface_name=wlan0 firmware_path=/system/etc/firmware/fw_RK903.bin nvram_path=/system/etc/firmware/nvram_RK903.cal"
+
+BOARD_HAVE_BLUETOOTH := true
---
+++ u30gt-common.mk
@@ -0,0 +1,102 @@
+#
+# Copyright (C) 2012 The Android Open-Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+DEVICE_PACKAGE_OVERLAYS := $(LOCAL_PATH)/overlay
+
+# This device is xhdpi.  However the platform doesn't
+# currently contain all of the bitmaps at xhdpi density so
+# we do this little trick to fall back to the hdpi version
+# if the xhdpi doesn't exist.
+PRODUCT_AAPT_CONFIG := normal hdpi xhdpi xlarge sw600dp sw720dp
+PRODUCT_AAPT_PREF_CONFIG := xlarge
+
+TARGET_BOOTANIMATION_NAME := horizontal-1280x800
+
+PRODUCT_COPY_FILES := \
+    $(LOCAL_PATH)/init.u30gt.rc:root/init.u30gt.rc \
+    $(LOCAL_PATH)/ueventd.u30gt.rc:root/ueventd.u30gt.rc \
+    $(LOCAL_PATH)/init.u30gt.usb.rc:root/init.u30gt.usb.rc
+    
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/vold.fstab:system/etc/vold.fstab \
+    $(LOCAL_PATH)/media_profiles.xml:system/etc/media_profiles.xml \
+    $(LOCAL_PATH)/keylayout/rk29-keypad.kl:system/usr/keylayout/rk29-keypad.kl
+    
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/firmware/fw_RK903.bin:system/etc/firmware/fw_RK903.bin \
+    $(LOCAL_PATH)/firmware/fw_RK903_p2p.bin:system/etc/firmware/fw_RK903_p2p.bin \
+    $(LOCAL_PATH)/firmware/fw_RK903_apsta.bin:system/etc/firmware/fw_RK903_apsta.bin \
+    $(LOCAL_PATH)/firmware/nvram_RK903.cal:system/etc/firmware/nvram_RK903.cal \
+    $(LOCAL_PATH)/firmware/nvram_RK903_26M.cal:system/etc/firmware/fw_RK903_26M.bin \
+    $(LOCAL_PATH)/firmware/rk903.hcd:system/etc/firmware/rk903.hcd \
+    $(LOCAL_PATH)/firmware/rk903_26M.hcd:system/etc/firmware/rk903_26M.hcd \
+    $(LOCAL_PATH)/firmware/nvram_RK903.txt:system/etc/firmware/nvram_RK903.txt
+    
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/bin/akmd8975:system/bin/akmd8975
+    
+PRODUCT_PROPERTY_OVERRIDES := \
+    wifi.interface=wlan0 \
+    wifi.supplicant_scan_interval=15
+
+PRODUCT_PACKAGES += \
+        audio.a2dp.default \
+        libaudioutils
+        
+# These are the hardware-specific features
+PRODUCT_COPY_FILES += \
+    frameworks/base/data/etc/tablet_core_hardware.xml:system/etc/permissions/tablet_core_hardware.xml \
+    frameworks/base/data/etc/android.hardware.camera.xml:system/etc/permissions/android.hardware.camera.xml \
+    frameworks/base/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
+    frameworks/base/data/etc/android.hardware.location.xml:system/etc/permissions/android.hardware.location.xml \
+    frameworks/base/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
+    frameworks/base/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
+    frameworks/base/data/etc/android.hardware.wifi.direct.xml:system/etc/permissions/android.hardware.wifi.direct.xml \
+    frameworks/base/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
+    frameworks/base/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
+    frameworks/base/data/etc/android.hardware.sensor.gyroscope.xml:system/etc/permissions/android.hardware.sensor.gyroscope.xml \
+    frameworks/base/data/etc/android.hardware.sensor.accelerometer.xml:system/etc/permissions/android.hardware.sensor.accelerometer.xml \
+    frameworks/base/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml \
+    frameworks/base/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
+    frameworks/base/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
+    frameworks/base/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml \
+    packages/wallpapers/LivePicker/android.software.live_wallpaper.xml:system/etc/permissions/android.software.live_wallpaper.xml
+
+PRODUCT_CHARACTERISTICS := tablet
+
+PRODUCT_TAGS += dalvik.gc.type-precise
+
+PRODUCT_PACKAGES += \
+    librs_jni \
+    com.android.future.usb.accessory
+    
+# Filesystem management tools
+PRODUCT_PACKAGES += \
+	make_ext4fs \
+	setup_fs
+
+DEVICE_PACKAGE_OVERLAYS := $(LOCAL_PATH)/overlay
+
+$(call inherit-product, frameworks/base/build/tablet-dalvik-heap.mk)
+
+
+PRODUCT_PROPERTY_OVERRIDES := \
+    service.adb.root=1 \
+    ro.secure=0 \
+    ro.allow.mock.location=1 \
+    ro.debuggable=1
+    
+    
